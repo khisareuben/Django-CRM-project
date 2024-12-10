9ea3f0cb391e3408114c8d996db88b99a6e0af4d
Khisa Reuben <khisareuben@gmail.com>
2024-12-10 10:21:15 +0300

    finished project

--- crm/forms.py
+++ crm/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from .models import Record
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -27,3 +28,26 @@ class CustomUserCreationForm(UserCreationForm):
             raise ValidationError("Passwords do not match.")
 
         return cleaned_data
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, label="First Name", widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    last_name = forms.CharField(required=True, label="Last Name", widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    email = forms.EmailField(required=True, label="Email", widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    phone = forms.CharField(required=True, label="Phone", widget=forms.TextInput(attrs={'placeholder': 'Phone', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    address = forms.CharField(required=True, label="Address", widget=forms.TextInput(attrs={'placeholder': 'Address', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    city = forms.CharField(required=True, label="City", widget=forms.TextInput(attrs={'placeholder': 'City', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    state = forms.CharField(required=True, label="State", widget=forms.TextInput(attrs={'placeholder': 'State', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    zipcode = forms.CharField(required=True, label="Zipcode", widget=forms.TextInput(attrs={'placeholder': 'Zipcode', 'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[600px] mb-[10px]'}))
+    
+    class Meta:
+      model = Record
+      exclude = ('user',)
+      
--- crm/views.py
+++ crm/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, AddRecordForm
 from django.contrib.auth.forms import UserCreationForm 
 from .models import Record 
 
@@ -70,7 +70,31 @@ def delete_record(request, pk):
     return redirect('home')
   
 def add_record(request):
-  return render(request, 'crm/add_record.html', {})
+  form = AddRecordForm(request.POST or None)
+  if request.user.is_authenticated:
+    if request.method == 'POST':
+      if form.is_valid():
+        add_record = form.save()
+        messages.success(request, "Record added successfully")
+        return redirect('home')     
+    
+    return render(request, 'crm/add_record.html', {'form': form})
+  else:
+    messages.success(request, 'You must be logged in ')
+    return redirect('home')
+  
+def update_record(request, pk):
+  if request.user.is_authenticated:
+    current_record = Record.objects.get(id=pk)
+    form = AddRecordForm(request.POST or None, instance=current_record)
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Record updated successfully')
+      return redirect('home')
+    return render(request, 'crm/update_record.html', {'form': form})
+  else:
+    messages.success(request, 'You must be logged in ')
+    return redirect('home')
 
 # def user_register(request):
 #   if request.method == 'POST':
